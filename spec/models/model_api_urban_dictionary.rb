e91c360e96bf5537ef2c36c165b3619afc2b2326
jakegibs617 <jakegibs617@gmail.com>
2017-09-17 22:14:00 -0400

    renaming table

--- spec/models/model_api_urban_dictionary.rb
+++ spec/models/model_api_urban_dictionary.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ModelForMyApi, type: :model do
+RSpec.describe ModelApiUrbanDictionary, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/search_words_spec.rb
+++ spec/models/search_words_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Greeting, type: :model do
+RSpec.describe SearchWords, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
